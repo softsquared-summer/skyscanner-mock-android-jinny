7ae8c87b990f2c6614e49b6a05b61041b7e383b9
JIeun <jieunan021@gmail.com>
2020-02-25 21:47:33 +0900

    [login] 로그인 첫화면 추가

--- app/src/main/java/com/jeahn/skyscanner/src/main/profile/ProfileFragment.java
+++ app/src/main/java/com/jeahn/skyscanner/src/main/profile/ProfileFragment.java
@@ -1,24 +1,28 @@
 package com.jeahn.skyscanner.src.main.profile;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
 import com.jeahn.skyscanner.R;
+import com.jeahn.skyscanner.src.main.profile.login.LoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileFragment extends Fragment implements NestedScrollView.OnScrollChangeListener {
+public class ProfileFragment extends Fragment implements NestedScrollView.OnScrollChangeListener, Button.OnClickListener {
     private Toolbar mToolbar;
     private NestedScrollView mScrollView;
     private View mToolbarDivider;
+    private Button mBtnLogin;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -33,7 +37,9 @@ public class ProfileFragment extends Fragment implements NestedScrollView.OnScro
         mToolbar = v.findViewById(R.id.profile_toolbar);
         mScrollView = v.findViewById(R.id.profile_scroll);
         mToolbarDivider = v.findViewById(R.id.profile_toolbar_divider);
+        mBtnLogin = v.findViewById(R.id.profile_btn_login);
 
+        mBtnLogin.setOnClickListener(this);
         mScrollView.setOnScrollChangeListener(this);
 
         return v;
@@ -47,6 +53,16 @@ public class ProfileFragment extends Fragment implements NestedScrollView.OnScro
         }else{
             mToolbar.setTitle(getString(R.string.profile_title));
             mToolbarDivider.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.profile_btn_login:
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                break;
         }
     }
 }
---
+++ app/src/main/java/com/jeahn/skyscanner/src/main/profile/login/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.jeahn.skyscanner.src.main.profile.login;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.URLSpan;
+import android.text.style.UnderlineSpan;
+import android.text.util.Linkify;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+
+import com.jeahn.skyscanner.R;
+import com.jeahn.skyscanner.src.BaseActivity;
+
+import java.util.regex.Pattern;
+
+public class LoginActivity extends BaseActivity {
+    private Toolbar mToolbar;
+    private TextView mTvTos;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mToolbar = findViewById(R.id.login_toolbar);
+        mTvTos = findViewById(R.id.login_tv_tos);
+
+        setToolbar();
+        setTosText();
+    }
+
+    //약관 링크
+    private void setTosText() {
+        Linkify.TransformFilter transform = (matcher, s) -> "";
+
+        Pattern pattern1 = Pattern.compile(getString(R.string.login_tos_pattern_1));
+        Pattern pattern2 = Pattern.compile(getString(R.string.login_tos_pattern_2));
+
+        Linkify.addLinks(mTvTos, pattern1, "https://www.skyscanner.co.kr/media/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%95%BD%EA%B4%80", null, transform);
+        Linkify.addLinks(mTvTos, pattern2, "https://www.skyscanner.co.kr/media/privacy-policy?previousCultureSource=COOKIE&redirectedFrom=www.skyscanner.net", null, transform);
+
+        //밑줄 제거
+        Spannable s = new SpannableString(mTvTos.getText());
+        for (URLSpan u : s.getSpans(0, s.length(), URLSpan.class)) {
+            s.setSpan(new UnderlineSpan() {
+                public void updateDrawState(TextPaint tp) {
+                    tp.setUnderlineText(false);
+                }
+            }, s.getSpanStart(u), s.getSpanEnd(u), 0);
+        }
+        mTvTos.setText(s);
+
+        mTvTos.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private void setToolbar() {
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        mToolbar.getNavigationIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //툴바의 <- 버튼 누르면 끝내기
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
